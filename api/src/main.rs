f1a987e8e3f5f27d73fbce46913e63493088ee56
scohen-trinity <scohen@trinity.edu>
2024-10-26 12:49:35 -0500

    feat: server runs on port 9000

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+use tower_http::cors::{Any, CorsLayer};
+
+#[tokio::main]
+async fn main() {
+    // create the CORS layer to allow requests
+    let cors: CorsLayer = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
+
+    // create the database connection pool
+    // let db_connection_str: String = std::env::var("DATABASE_URL")
+    //     .unwrap_or_else(|_| "postgres://postgres:password@localhost".to_string());
+
+    // TODO create the pool
+
+    // application routes
+    let app: Router = Router::new()
+        .route("/", get(root))
+        .layer(cors);
+
+    // listen globally to port 9000
+    let listener: TcpListener = tokio::net::TcpListener::bind("0.0.0.0:9000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
+
+async fn root() -> &'static str { "Hello Axum!" }
