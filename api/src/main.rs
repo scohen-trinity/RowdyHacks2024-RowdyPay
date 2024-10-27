ba1353b1e6d34c5f66689716a2cabbe4b4d00eb3
scohen-trinity <scohen@trinity.edu>
2024-10-26 19:38:26 -0500

    feat: got database implementation to work

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,5 +1,6 @@
 use axum::{routing::get, Router};
 use controllers::{balance_controller::balance_routes, group_controller::group_routes, hello_world_controller::hello_world_routes, payment_controller::payment_routes, profile_controller::profile_routes};
+use sqlx::postgres::PgPoolOptions;
 use tokio::net::TcpListener;
 use tower_http::cors::{Any, CorsLayer};
 
@@ -12,13 +13,27 @@ async fn main() {
         .allow_headers(Any);
 
     // create the database connection pool
-    // let db_connection_str: String = std::env::var("DATABASE_URL")
-    //     .unwrap_or_else(|_| "postgres://postgres:password@localhost".to_string());
+    let db_connection_str: String = std::env::var("DATABASE_URL")
+        .unwrap_or_else(|_| "postgres://postgres:password@localhost".to_string());
 
-    // TODO create the pool
+    // create the database pool
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_connection_str)
+        .await
+        .expect("couldn't connect to the database");
+
+    // testing the database with a simple query
+    let row: (String,) = sqlx::query_as("SELECT version()")
+        .fetch_one(&pool)
+        .await
+        .expect("Failed to execute test query");
+
+    println!("Connection is working, psql version: {}", row.0);
 
     // application routes
     let app: Router = Router::new()
+        .with_state(pool)
         .route("/", get(root))
         .nest("/api", hello_world_routes())
         .nest("/api", balance_routes())
